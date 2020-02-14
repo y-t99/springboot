0ff5d7f68f28f6b988c6f193aba857bd348b9896
yuanyuan <13060889537@sina.cn>
2020-02-14 09:27:28 +0800

    init project

---
+++ src/main/java/cn/yuanyuan/YtApplicationLanch.java
@@ -0,0 +1,15 @@
+package cn.yuanyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuanyuan
+ * #create 2020-02-14-9:26
+ */
+@SpringBootApplication
+public class YtApplicationLanch {
+    public static void main(String[] args) {
+        SpringApplication.run(YtApplicationLanch.class);
+    }
+}
